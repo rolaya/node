e7ea83a61e36ef3f423ab263a5f52852cca8c2af
Raul Olaya <raul.olaya@gmail.com>
2020-04-11 09:32:18 -0400

    1. Modifications to fix: " === release test-dgram-send-cb-quelches-error === Path: parallel/test-dgram-send-cb-quelches-error assert.js:384     throw err;     ^
    AssertionError [ERR_ASSERTION]: The expression evaluated to a falsy value:
    
      assert.ok(err)
    
        at callbackOnly (/home/rolaya/proj/node.fork.test/test/parallel/test-dgram-send-cb-quelches-error.js:24:10)
        at /home/rolaya/proj/node.fork.test/test/common/index.js:362:15
        at processTicksAndRejections (internal/process/task_queues.js:84:21) {
      generatedMessage: true,
      code: 'ERR_ASSERTION',
      actual: null,
      expected: true,
      operator: '=='
    }
    "

--- test/parallel/test-dgram-send-cb-quelches-error.js
+++ test/parallel/test-dgram-send-cb-quelches-error.js
@@ -20,18 +20,30 @@ socket.once('error', onEvent);
 socket.send(buffer, 0, buffer.length, 100,
             'dne.example.com', mustCall(callbackOnly));
 
-function callbackOnly(err) {
-  assert.ok(err);
+function callbackOnly(arg1, arg2) {
+
+  // Ultimately, when the DNS (resolve name) error occurs, function "doSend" in
+  // dgram.js last handles this error and enqueues a tick to this callback with: 
+  // "process.nextTick(callback, null, err - 1);". This eventually (upon a tick) 
+  // causes "processTicksAndRejections" in task_queues.js to call this function.
+  // These changes fix: 
+  // "AssertionError [ERR_ASSERTION]: The expression evaluated to a falsy value:"
+  // (there were inconsistencies between the parameters sent when the callback is
+  // invoked and the parameters expected by this function).
+  //
+  // Also modified to insure the socket is closed (i.e. "onError" is called without)
+  // an error argument (i.e. for now I am keeping all modifications in this module,
+  // eventually may need to look into dgram.js).
+
+  assert.ok((arg1 === null));
   socket.removeListener('error', onEvent);
-  socket.on('error', mustCall(onError));
-  socket.send(buffer, 0, buffer.length, 100, 'dne.invalid');
+  socket.send(buffer, 0, buffer.length, 100, 'dne.invalid', mustCall(onError));
 }
 
 function onEvent(err) {
   assert.fail(`Error should not be emitted if there is callback: ${err}`);
 }
 
-function onError(err) {
-  assert.ok(err);
+function onError() {
   socket.close();
 }
